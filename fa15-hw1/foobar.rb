8011315c66d6ff0e0fccc7b6f4725191535ee6b2
4148 <angus@angusshire.com>
2015-09-17 15:48:44 -0700

    hw1 d

--- fa15-hw1/foobar.rb
+++ fa15-hw1/foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+    a.uniq.inject(0) do |sum,x| # inject(0) initializes default to 0 insteado f
+      # 1st elm
+      if x.to_i.even? and x.to_i < 10
+        sum.to_i + x.to_i + 2
+      else
+        sum.to_i
+      end
+    end
+  end
 end
